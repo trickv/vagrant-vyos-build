8204e10868ce5da822c4daf9bdb61a47052a917e
Patrick van Staveren <trick@vanstaveren.us>
2016-01-28 23:51:00 +0000

    install base build packages and git+vim so we can hack, and check out a copy of the source code

--- init.sh
+++ init.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-echo hello world
+apt-get update
+apt-get -y dist-upgrade
+
+apt-get -y install build-essential python-pystache live-build
+
+apt-get -y install git vim
+
+
+if [ ! -d ~vagrant/vyos-build ]; then
+    su -c "git clone https://github.com/vyos/vyos-build" vagrant
+fi
