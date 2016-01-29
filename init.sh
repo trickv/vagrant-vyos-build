26c7d12d9382d5e04325addfee8d2181c922de9d
Patrick van Staveren <trick@vanstaveren.us>
2016-01-29 09:17:27 +0000

    make setup work with an aws vm. ready to go.
    * catch errors on setup
    * suppress verbose logging with apt-get -q
    * work with either vagrant or admin username.

--- init.sh
+++ init.sh
@@ -1,15 +1,26 @@
 #!/usr/bin/env bash
 
+set -u
+set -e
+
+sudo apt-mark hold grub-pc # to prevent config prompts on EC2 VMs
+
 apt-get update
-apt-get -y dist-upgrade
 
-apt-get -y install build-essential python-pystache live-build
+apt-get -q -y dist-upgrade
 
-apt-get -y install git vim
+apt-get -q -y install build-essential python-pystache live-build
 
+apt-get -q -y install git vim
 
-if [ ! -d ~vagrant/vyos-build ]; then
-    su -c "git clone https://github.com/vyos/vyos-build" vagrant
+if [ -d ~vagrant ]; then
+    vagrant_user=vagrant
+elif [ -d ~admin ]; then
+    vagrant_user=admin
+fi
+
+if [ "$vagrant_user" != "" -a ! -d ~$vagrant_user/vyos-build ]; then
+    su -c "git clone https://github.com/vyos/vyos-build" $vagrant_user
 fi
 
 echo "To build your VyOS iso:
